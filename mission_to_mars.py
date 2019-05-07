3e87330ee44cf1354530d7a3aa03427c738b3635
WCW789 <wcw789@gmail.com>
2019-05-07 01:53:55 -0500

    Added

--- mission_to_mars.py
+++ mission_to_mars.py
@@ -48,8 +48,37 @@ def scrape():
     #     'table', class_='tablepress tablepress-id-mars')
     tables = pd.read_html(url4)
     df = tables[0]
-    df.rename(columns={0:'Key', 1:'Fact'}, inplace=True)
+    df.rename(columns={0: 'Key', 1: 'Fact'}, inplace=True)
     html_table = df.to_html()
     listings["table"] = html_table.replace('\n', '')
+
+    url5_1 = 'https://astrogeology.usgs.gov/search/map/Mars/Viking/valles_marineris_enhanced'
+    browser.visit(url5_1)
+    html5_1 = browser.html
+    soup5_1 = BeautifulSoup(html5_1, 'html.parser')
+    image5_1 = soup5_1.find_all('img', class_='wide-image')[0]["src"]
+
+    url5_2 = 'https://astrogeology.usgs.gov/search/map/Mars/Viking/cerberus_enhanced'
+    browser.visit(url5_2)
+    html5_2 = browser.html
+    soup5_2 = BeautifulSoup(html5_2, 'html.parser')
+    image5_2 = soup5_2.find_all('img', class_='wide-image')[0]["src"]
+
+    url5_3 = 'https://astrogeology.usgs.gov/search/map/Mars/Viking/schiaparelli_enhanced'
+    browser.visit(url5_3)
+    html5_3 = browser.html
+    soup5_3 = BeautifulSoup(html5_3, 'html.parser')
+    image5_3 = soup5_3.find_all('img', class_='wide-image')[0]["src"]
+
+    url5_4 = 'https://astrogeology.usgs.gov/search/map/Mars/Viking/syrtis_major_enhanced'
+    browser.visit(url5_4)
+    html5_4 = browser.html
+    soup5_4 = BeautifulSoup(html5_4, 'html.parser')
+    image5_4 = soup5_4.find_all('img', class_='wide-image')[0]["src"]
+
+    listings["img1"] = "https://astrogeology.usgs.gov/" + image5_1
+    listings["img2"] = "https://astrogeology.usgs.gov/" + image5_2
+    listings["img3"] = "https://astrogeology.usgs.gov/" + image5_3
+    listings["img4"] = "https://astrogeology.usgs.gov/" + image5_4
 
     return listings
